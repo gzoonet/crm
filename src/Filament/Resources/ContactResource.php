86758971d2b6cded67460bf56f9ead821e6a8312
Eddy <eddy@gzoo.net>
2025-05-14 14:45:20 -0700

    Update package

--- src/Filament/Resources/ContactResource.php
+++ src/Filament/Resources/ContactResource.php
@@ -1,71 +1,133 @@
 <?php
 
-namespace Gzoonet\Crm;
+namespace Gzoonet\Crm\Filament\Resources;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Filament\Facades\Filament;
-use Gzoonet\Crm\Filament\Resources\CustomerResource;
-use Gzoonet\Crm\Filament\Resources\ContactResource;
-use Gzoonet\Crm\Filament\Resources\LeadResource;
-use Gzoonet\Crm\Filament\Resources\TaskResource;
-use Gzoonet\Crm\Filament\Resources\TagResource;
-use Gzoonet\Crm\Filament\Pages\Dashboard;
-use Gzoonet\Crm\Filament\Widgets\ActiveLeadsByStageWidget;
-use Gzoonet\Crm\Filament\Widgets\TasksDueThisWeekWidget;
-use Gzoonet\Crm\Filament\Widgets\RecentActivityWidget;
-use Gzoonet\Crm\Filament\Widgets\NewCustomersThisMonthWidget;
-use Gzoonet\Crm\Filament\Widgets\ConversionRateWidget;
+use Gzoonet\Crm\Filament\Resources\ContactResource\Pages;
+use Gzoonet\Crm\Filament\Resources\RelationManagers\NotesRelationManager;
+use Gzoonet\Crm\Models\Contact;
+use Gzoonet\Crm\Models\Customer;
+use Gzoonet\Crm\Models\Tag;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TagsInput;
+use Filament\Tables\Filters\SelectFilter;
 
 class ContactResource extends Resource
 {
-    public function configurePackage(Package $package): void
+    protected static ?string $model = Contact::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-identification';
+
+    protected static ?string $navigationGroup = 'CRM';
+
+    public static function form(Form $form): Form
     {
-        $package
-            ->name("gzoonet-crm")
-            ->hasConfigFile()
-            ->hasMigrations([
-                "create_customers_table",
-                "create_contacts_table",
-                "create_tasks_table",
-                "create_notes_table",
-                "create_tags_table",
-                "create_taggables_table",
-                "create_leads_table",
+        return $form->schema([
+            Forms\Components\Select::make('customer_id')
+                ->relationship('customer', 'name')
+                ->searchable()
+                ->preload()
+                ->required(),
+            Forms\Components\TextInput::make('first_name')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('last_name')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('email')
+                ->email()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('phone')
+                ->tel()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('position')
+                ->maxLength(255),
+            Forms\Components\Textarea::make('notes')
+                ->columnSpanFull(),
+            TagsInput::make('tags')
+                ->label('Tags')
+                ->suggestions(
+                    Tag::all()->pluck('name')->toArray()
+                )
+                ->relationship('tags', 'name')
+                ->columnSpanFull(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('customer.name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('first_name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('last_name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('email')->searchable(),
+                Tables\Columns\TextColumn::make('phone')->searchable(),
+                Tables\Columns\TextColumn::make('position')->searchable(),
+                Tables\Columns\TextColumn::make('tags.name')
+                    ->label('Tags')
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+                SelectFilter::make('tags')
+                    ->relationship('tags', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->label('Tags'),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
             ]);
     }
 
-    public function packageRegistered(): void
+    public static function getRelations(): array
     {
-        // You can bind things to the container here, if needed.
+        return [
+            NotesRelationManager::class,
+        ];
     }
 
-    public function packageBooted(): void
+    public static function getPages(): array
     {
-        if (class_exists(Filament::class)) {
-            Filament::registerNavigationGroups([
-                'CRM',
-            ]);
+        return [
+            'index' => Pages\ListContacts::route('/'),
+            'create' => Pages\CreateContact::route('/create'),
+            'view' => Pages\ViewContact::route('/{record}'),
+            'edit' => Pages\EditContact::route('/{record}/edit'),
+        ];
+    }
 
-            Filament::registerResources([
-                CustomerResource::class,
-                ContactResource::class,
-                LeadResource::class,
-                TaskResource::class,
-                TagResource::class,
-            ]);
-
-            Filament::registerPages([
-                Dashboard::class,
-            ]);
-
-            Filament::registerWidgets([
-                ActiveLeadsByStageWidget::class,
-                TasksDueThisWeekWidget::class,
-                RecentActivityWidget::class,
-                NewCustomersThisMonthWidget::class,
-                ConversionRateWidget::class,
-            ]);
-        }
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->withoutGlobalScopes([
+            SoftDeletingScope::class,
+        ]);
     }
 }
